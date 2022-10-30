d47d5be7d8b1bb8d364c860940ea6be5b2fe240a
raghu <58185871+raghavendramallela@users.noreply.github.com>
2022-10-30 12:33:39 +0530

    Update 2optionaleditlimits.sh

--- 2optionaleditlimits.sh
+++ 2optionaleditlimits.sh
@@ -1,11 +1,11 @@
 # optional edit processes limits
 echo " # Default limit for number of user's processes to prevent
-	# accidental fork bombs.# See rhbz #432903 for reasoning.
-  *    soft  nproc  131070
-	*    hard  nproc  131070
-	*    soft  nofile 131070
-	*    hard  nofile 131070
-	root  soft  nproc  unlimited
-  stack soft  nproc  unlimited " | sudo tee /etc/security/limits.d/20-nproc.conf
+# accidental fork bombs.# See rhbz #432903 for reasoning.
+*    soft  nproc  131070
+*    hard  nproc  131070
+*    soft  nofile 131070
+*    hard  nofile 131070
+root  soft  nproc  unlimited
+stack soft  nproc  unlimited " | sudo tee /etc/security/limits.d/20-nproc.conf
   
 sudo reboot  
